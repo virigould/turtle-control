9462d5b8dc31283dc31d97e02f4d94a08eee3f44
virigould <virigould@gmail.com>
2025-04-24 21:50:31 -0400

    adding initial files

---
+++ server.py
@@ -0,0 +1,78 @@
+import asyncio
+import websockets
+import json
+
+clients = {}
+
+
+async def send_command(websocket, command):
+    await websocket.send(json.dumps({'type': 'eval', 'command': 'return ' + command}))
+    return json.loads(await websocket.recv())
+
+
+async def send_inspect(websocket, direction):
+    await websocket.send(json.dumps({'type': 'inspect', 'direction': direction}))
+    return json.loads(await websocket.recv())
+
+async def send_dig(websocket, direction):
+    await websocket.send(json.dumps({'type': 'dig', 'direction': direction}))
+    return json.loads(await websocket.recv())
+
+async def check_inventory(websocket):
+    await websocket.send(json.dumps({'type': 'inventory'}))
+    return json.loads(await websocket.recv())
+
+async def send_place(websocket, direction):
+    await websocket.send(json.dumps({'type': 'place', 'direction': direction}))
+    return json.loads(await websocket.recv())
+
+async def send_move(websocket, direction):
+    await websocket.send(json.dumps({'type': 'move', 'direction': direction}))
+    return json.loads(await websocket.recv())
+
+async def send_suck(websocket, direction, count=None):
+    if count is not None:
+        await websocket.send(json.dumps({'type': 'move', 'direction': direction, 'count': count}))
+        return json.loads(await websocket.recv())
+    await websocket.send(json.dumps({'type': 'move', 'direction': direction}))
+    return json.loads(await websocket.recv())
+
+async def send_turn(websocket, direction):
+    await websocket.send(json.dumps({'type': 'turn', 'direction': direction}))
+    return json.loads(await websocket.recv())
+
+async def go_mining(websocket):
+    location = await send_command(websocket, 'turtle.turnLeft()')
+    block = await send_inspect(websocket, 'down')
+    print(block)
+
+
+async def handle_client(websocket):
+
+    first_message = await websocket.recv()
+    client_id = json.loads(first_message)['name']
+    print("just got the first message")
+
+    try:
+        async for message in websocket:
+
+            if client_id not in clients:
+                clients[client_id] = websocket
+
+
+            #await websocket.send(json.dumps({'type': 'eval', 'command': 'turtle.turnLeft()'}))
+            await go_mining(websocket)
+            print("Just sent the test message")
+
+    except websockets.exceptions.ConnectionClosed as e:
+        print(f"Disconnected {client_id}")
+    finally:
+        del clients[client_id]
+
+
+async def main():
+    async with websockets.serve(handle_client, "localhost", 7788):
+        await asyncio.Future()  # run forever
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ startserver.lua
@@ -0,0 +1,260 @@
+
+local random = math.random
+local ws, err = http.websocket("ws://localhost:7788")
+local computer_info = nil
+
+
+local function uuid()
+    local template ='xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
+    return string.gsub(template, '[xy]', function (c)
+        local v = (c == 'x') and random(0, 0xf) or random(8, 0xb)
+        return string.format('%x', v)
+    end)
+end
+
+function file_exists(name)
+    local f = io.open(name, "r")
+    return f ~= nil and io.close(f)
+end
+
+function check_inventory()
+    local inventory = {}
+    for i=1,9,1
+    do
+        
+        local item = turtle.getItemDetail(i)
+        inventory[item.name] = item.count
+        return inventory
+end
+
+
+function send_message(message)
+    ws.send(string.format("{\"name\": \"%s\", \"message\": \"%s\"}", computer_info, message))
+end
+
+function handle_message(message)
+    local info = textutils.unserializeJSON(message)
+    if info["type"] == "eval" then
+        print(info["command"])
+        local f = load(info["command"])
+        local command_output = f()
+        print(textutils.serializeJSON(command_output))
+        ws.send(textutils.serializeJSON(command_output))
+        do return end
+    elseif info["type"] == "inspect" then
+        print("Inpecting" .. info["direction"])
+        if info["direction"] == "down" then
+            local bool, block_info = turtle.inspectDown()
+            if bool then
+                ws.send(textutils.serializeJSON(block_info))
+                do return end
+            end
+        elseif info["direction"] == "forward" then
+            local bool, block_info = turtle.inspect()
+            if bool then
+                ws.send(textutils.serializeJSON(block_info))
+                do return end
+            end
+        elseif info["direction"] == "up" then
+            local bool, block_info = turtle.inspectUp()
+            if bool then
+                ws.send(textutils.serializeJSON(block_info))
+                do return end
+            end
+        else
+            ws.send("You need to turn dumbass?")
+            do return end
+        end
+    elseif info["type"] == "inventory" then
+        ws.send(textutils.serializeJSON(check_inventory()))
+        do return end
+    elseif info["type"] == "gps" then
+        local postion = vector.new(gps.locate(5))
+        ws.send(textutils.serializeJSON(positon))
+        do return end
+
+    elseif info["type"] == "turn" then
+        if info["direction"] == "left" then
+            ws.send(textutils.serializeJSON(turtle.turnLeft()))
+            do return end
+        else 
+            ws.send(textutils.serializeJSON(turtle.turnRight()))
+            do return end
+        end
+
+    elseif info["type"] == "dig" then
+        if info["direction"] == "down" then
+            local bool, mssg = turtle.digDown("right")
+            if not bool then
+                ws.send(textutils.serializeJSON(mssg))
+                do return end
+            end
+        elseif info["direction"] == "forward" then
+            local bool, mssg = turtle.dig("right")
+            if not bool then
+                ws.send(textutils.serializeJSON(mssg))
+                do return end
+            end
+        elseif info["direction"] == "up" then
+            local bool, mssg  = turtle.digUp("right")
+            if not bool then
+                ws.send(textutils.serializeJSON(mssg))
+                do return end
+            end
+        else
+            ws.send("You need to turn dumbass?")
+            do return end
+        end
+
+    elseif info["type"] == "suck" then
+        if info["direction"] == "down" then
+            local bool, mssg = nil, nil
+            if not info["count"] then
+                bool, mssg = turtle.suckDown()
+            else
+                bool, mssg = turtle.suckDown(info["count"])
+            end
+            if not bool then
+                ws.send(textutils.serializeJSON(mssg))
+                do return end
+            end
+        elseif info["direction"] == "forward" then
+            local bool, mssg = nil, nil
+            if not info["count"] then
+                bool, mssg = turtle.suck()
+            else
+                bool, mssg = turtle.suck(info["count"])
+            end
+            if not bool then
+                ws.send(textutils.serializeJSON(mssg))
+                do return end
+            end
+        elseif info["direction"] == "up" then
+            local bool, mssg = nil, nil
+            if not info["count"] then
+                bool, mssg = turtle.suckUp()
+            else
+                bool, mssg = turtle.suckUp(info["count"])
+            end
+            if not bool then
+                ws.send(textutils.serializeJSON(mssg))
+                do return end
+            end
+        else
+            ws.send("You need to turn dumbass?")
+            do return end
+        end
+
+    elseif info["type"] == "place" then
+        if info["direction"] == "down" then
+            local bool, mssg = turtle.placeDown()
+            if not bool then
+                ws.send(textutils.serializeJSON(mssg))
+                do return end
+            end
+        elseif info["direction"] == "forward" then
+            local bool, mssg = turtle.place()
+            if not bool then
+                ws.send(textutils.serializeJSON(mssg))
+                do return end
+            end
+        elseif info["direction"] == "up" then
+            local bool, mssg  = turtle.placeUp()
+            if not bool then
+                ws.send(textutils.serializeJSON(mssg))
+                do return end
+            end
+        else
+            ws.send("You need to turn dumbass?")
+            do return end
+        end
+
+    elseif info["type"] == "refuel" then
+        if info["slot"] ~= nil then
+            local fueled, mssg = turtle.refuel()
+            if not fueled then
+                ws.send(textutils.serializeJSON({message=mssg}))
+            else
+                ws.send(textutils.serializeJSON(turtle.getFuelLevel()))
+            end
+
+        else
+            turtle.select(info["slot"])
+            local fueled, mssg = turtle.refuel()
+            if not fueled then
+                ws.send(textutils.serializeJSON({message=mssg}))
+            else
+                ws.send(textutils.serializeJSON(turtle.getFuelLevel()))
+            end
+        end
+    
+    elseif info["type"] == "move" then
+        if info["direction"] == "forward" then    
+            local move_attempt, mssg = turtle.forward()
+            if not move_attempt then 
+                ws.send(textutils.serializeJSON(mssg))
+                do return end
+            else
+                ws.send(textutils.serializeJSON({bool=move_attempt, fuel=turtle.getFuelLevel() .. "/" .. turtle.getFuelLimit()}))
+                do return end
+            end
+        elseif info["direction"] == "back" then
+            local move_attempt, mssg = turtle.back()
+            if not move_attempt then 
+                ws.send(textutils.serializeJSON(mssg))
+                do return end
+            else
+                ws.send(textutils.serializeJSON({bool=move_attempt, fuel=turtle.getFuelLevel() .. "/" .. turtle.getFuelLimit()}))
+                do return end
+            end
+        elseif info["direction"] == "up" then
+            local move_attempt, mssg = turtle.up()
+            if not move_attempt then 
+                ws.send(textutils.serializeJSON(mssg))
+                do return end
+            else
+                ws.send(textutils.serializeJSON({bool=move_attempt, fuel=turtle.getFuelLevel() .. "/" .. turtle.getFuelLimit()}))
+                do return end
+            end
+        elseif info["direction"] == "down" then
+            local move_attempt, mssg = turtle.down()
+            if not move_attempt then 
+                ws.send(textutils.serializeJSON(mssg))
+                do return end
+            else
+                ws.send(textutils.serializeJSON({bool=move_attempt, fuel=turtle.getFuelLevel() .. "/" .. turtle.getFuelLimit()}))
+                do return end
+            end
+        end
+        
+    elseif info["type"] == "test" then
+        print("It got the test command")
+    else
+        print("Could not understand the command")
+    end
+    ws.send("Ready")
+end
+
+
+function main()
+    
+    
+    if file_exists("computer_info.txt") then
+        local file = io.open("computer_info.txt", "r")
+        computer_info = textutils.unserialize(file:read "*all")
+    else
+        computer_info = uuid()
+        local file = io.open("computer_info.txt", "w")
+        file:write(textutils.serialize(computer_info))
+    end
+    ws.send(string.format("{\"name\": \"%s\"}", computer_info))
+    ws.send("ready")
+    while true do
+        local message = ws.receive()
+        handle_message(message)
+    end
+end
+
+
+main()
+
