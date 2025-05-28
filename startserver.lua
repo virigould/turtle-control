1b30b517014a192175d0c00bb2eb159dbf24dac7
c3llkn1ght <164270614+c3llkn1ght@users.noreply.github.com>
2025-05-27 23:55:29 -0400

    uhhhhhhmmmmmmmm

--- startserver.lua
+++ startserver.lua
@@ -314,6 +314,8 @@ function handle_message(message)
 	elseif info["type"] == "refuel" then
 		if info["slot"] ~= nil then
 			local fueled, mssg = turtle.refuel()
+			print(fueled)
+			print(mssg)
 			if not fueled then
 				local issue = { command_output = mssg, command_id = info["id"] }
 				ws.send(textutils.serializeJSON(issue))
